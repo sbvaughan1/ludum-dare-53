432aabdf08d6100064cd560bc040af1844e9d33c
Wyatt Beavers <hammerfrenzy@gmail.com>
2023-04-29 00:56:59 -0500

    Update camera when changing rat control

--- Assets/Scripts/Character/RatSwapperBehavior.cs
+++ Assets/Scripts/Character/RatSwapperBehavior.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RatSwapperBehavior : MonoBehaviour
 {
     public List<RatControllerBehavior> RatControllers;
+
+    private CameraTargetBehavior[] cameraTargets;
     private int controlledIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         RatControllers[controlledIndex].ChangeControl(true);
+        cameraTargets = FindObjectsOfType<CameraTargetBehavior>();
     }
 
     // Update is called once per frame
@@ -18,17 +22,49 @@ public class RatSwapperBehavior : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            controlledIndex++;
-            if (controlledIndex >= RatControllers.Count)
-            {
-                controlledIndex = 0;
-            }
+            ActivateNextRat();
+        }
+    }
 
-            for (int i = 0; i < RatControllers.Count; i++)
+    private void ActivateNextRat()
+    {
+        controlledIndex++;
+        if (controlledIndex >= RatControllers.Count)
+        {
+            controlledIndex = 0;
+        }
+
+        for (int i = 0; i < RatControllers.Count; i++)
+        {
+            var ratController = RatControllers[i];
+            var giveControl = controlledIndex == i;
+            ratController.ChangeControl(giveControl);
+
+            // Activate the closest camera target to this rat
+            if (giveControl)
             {
-                var ratController = RatControllers[i];
-                ratController.ChangeControl(controlledIndex == i);
+                ActivateCameraClosestToRat(ratController);
             }
         }
+    }
+
+    private void ActivateCameraClosestToRat(RatControllerBehavior rat)
+    {
+        CameraTargetBehavior closestTarget = null;
+        var closestDistance = float.MaxValue;
+        var ratPosition = rat.transform.position;
+        for (int i = 0; i < cameraTargets.Length; i++)
+        {
+            var cameraTarget = cameraTargets[i];
+            var distance = Vector3.Distance(ratPosition, cameraTarget.transform.position);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestTarget = cameraTarget;
+            }
+        }
+
+        Camera.main.transform.position = closestTarget.transform.position;
     }
 }
